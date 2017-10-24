3b14f9edaf129428f8d8d42362082f466d04e989
Martin Bing <bing.martin.photography@gmail.com>
2017-10-24 16:41:12 +0200

    Add a circle

--- js/script.js
+++ js/script.js
@@ -5,4 +5,10 @@ context.beginPath();
 context.rect(20, 40, 50, 50);
 context.fillStyle='#FF0000';
 context.fill();
+context.c
+
+context.beginPath();
+context.arc(240, 160, 20, 0, Math.PI*2, false);
+context.fillStyle='green';
+context.fill();
 context.closePath();
