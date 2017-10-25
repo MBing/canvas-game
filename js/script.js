7af539453ceae580a27ef977bb7828255b366ec9
Martin Bing <bing.martin.photography@gmail.com>
2017-10-25 09:04:08 +0200

    Create playable game with bottom pad and keys

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 var canvas = document.getElementById('my-canvas');
 var context = canvas.getContext('2d');
 
-setInterval(draw, 10);
-
 var x = canvas.width / 2;
 var y = canvas.height -30;
 
@@ -10,6 +8,31 @@ var dx = 2;
 var dy = -2;
 
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+function keyDownHandler(e) {
+    console.log(e.keyCode)
+    if (e.keyCode === 39) {
+        rightPressed = true;
+    } else if (e.keyCode === 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode === 39) {
+        rightPressed = false;
+    } else if (e.keyCode === 37) {
+        leftPressed = false;
+    }
+}
 
 function drawBall() {
     context.beginPath();
@@ -19,18 +42,42 @@ function drawBall() {
     context.closePath();
 }
 
+function drawPaddle() {
+    context.beginPath();
+    context.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    context.fillStyle = 'green';
+    context.fill();
+    context.closePath();
+}
+
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();    
 
-    if (y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
+    if (y + dy < ballRadius) {
         dy = -dy;
-    } 
+    } else if (y + dy > canvas.height-ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            // alert('game over');
+            document.location.reload();
+        }
+    }
     
     if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
         dx = -dx;
     }
 
+    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
     x += dx;
     y += dy;
 }
+
+setInterval(draw, 10);
